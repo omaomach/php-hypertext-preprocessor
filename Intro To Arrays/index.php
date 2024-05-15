1d03ce438b765a7e901f51d64eefd0994d79e00c
omaomach <aaronjoash44@gmail.com>
2024-05-15 11:01:18 +0300

    working on arrays

---
+++ Intro To Arrays/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$names = array('John', 'Jack', 'Jill');
+$numbers = [1, 2, 3, 4, 5, 6, 7];
+
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    // die(); // this will prevent anything below it from being rendered
+}
+
+// inspect($names);
+// inspect($numbers);
+
+// print_r($names);
+
+// echo $names[1] . '<br />';
+// echo $numbers[5];
+
+// Add an element to an array
+$numbers[6] = 100;
+$numbers[] = 102;
+
+// Delete an element from an array
+unset($numbers[3]); // This function deletes the element as well as the index.
+$numbers = array_values($numbers); // We use this function to reset the array i.e reorder the indexes
+
+inspect($numbers);
