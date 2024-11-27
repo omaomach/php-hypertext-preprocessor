e674677eb40f2b6dfd9c3efa0f20ab18ff6e590b
omaomach <aaronjoash44@gmail.com>
2024-11-27 17:08:37 +0300

    worked on global and local scope

---
+++ Functions/Global & Local Scope/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Global Scope
+$name = 'Alice';
+
+function sayHello()
+{
+    global $name;
+
+    // Local Scope
+    $name = 'Joel';
+    echo 'Hello ' . $name;
+}
+
+// echo $name;
+
+// sayHello();
+
+// echo $name;
+
+function sayGoodbye()
+{
+    $names = ['Jack', 'Jill'];
+    echo 'Goodbye ' . $names[0];
+}
+
+sayGoodbye();
